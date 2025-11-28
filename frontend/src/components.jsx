014efb65519482fae0e3a2371bbae5d3c0666757
Aayush <mishraayu185@gmail.com>
2025-11-28 08:02:59 -0800

    adding marquee tag

--- frontend/src/components.jsx
+++ frontend/src/components.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function components() {
   return (
-    <div><h1>hello combo main</h1></div>
+    <div><marquee behavior="4" direction="right"><h1>hello combo main</h1></marquee></div>
   )
 }
 
