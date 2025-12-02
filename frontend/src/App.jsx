e8077126c33100dee91b7ff4e72c4cee8984633a
Aayush <mishraayu185@gmail.com>
2025-12-02 03:19:56 -0800

    update src folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,38 @@
 import { useState,useEffect } from 'react';
-import React from 'react';
-import axios from 'axios';
 
+import axios from 'axios';
+import Cars from './components';
 import './App.css'
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+
+
+import React from 'react'
 
 function App() {
-  const [data, setData] = useState("");
-
-  useEffect(() => {
-    axios.get("http://127.0.0.1:8000/api/hello/")
-    .then(response => {setData(response.data.message)
-      setLoading(false);
-    })
-
-    .catch(error => document.error("There was an error!", error));
-  },[]);
-
   return (
-    <>
-    
-    
-    <h1>
-      <h1>data</h1>
-      <mark>{data}</mark>
-    </h1>
-      
-    </>
+    <div>
+      <BrowserRouter>
+      {/* Navigation */}
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/products">Products</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />}>
+          <Route path="car" element={<CarProducts />} />
+          <Route path="bike" element={<BikeProducts />} />
+        </Route>
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+
+    </div>
   )
 }
+
 
 export default App
--- frontend/src/components.jsx
+++ frontend/src/components.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useState } from 'react';
+import { Suspense }  from 'react';
 
-function components() {
+function MyCars() {
+  // const cars = [ {id: 1001, brand: 'Ford'},
+  //   {id: 1002, brand: 'BMW'},
+  //   {id: 1003, brand: 'Audi'}];
+  // const[myval,setMyval] = useState("");
+  // function getting(e){
+  //   setMyval(e.target.value);
+  // }
   return (
-    <div><marquee behavior="4" direction="right"><h1>hello combo main</h1></marquee></div>
-  )
+    <>
+     {/* list 
+      <ul>
+        {cars.map((car)=><li key={car.id}>{car.id} {car.brand}</li>)}
+      </ul>*/}
+      {/* <textarea value={myval} onChange={getting} id=""></textarea>
+      <h1>{myval}</h1> */}
+      
+    </>
+  );
 }
 
-export default components
+export default MyCars
