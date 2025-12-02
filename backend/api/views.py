857368558203cb2d20755555f662caae76bd491c
Aayush <mishraayu185@gmail.com>
2025-12-01 23:32:27 -0800

    test commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+#here we define a simple view that returns a "hello django!" message
 def hello (request):
-    return HttpResponse("hello django!")
+    return HttpResponse("hello django! whts up")
