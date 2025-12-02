e0132220e0dea49aee569bc2e544447103d91b12
Aayush Mishra <164762713+AayushMishragit@users.noreply.github.com>
2025-12-01 23:34:47 -0800

    Refactor hello view and clean up comments
    Removed unnecessary comment and fixed spacing in the hello view.

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-#here we define a simple view that returns a "hello django!" message pull it now here
+
 def hello (request):
     return HttpResponse("hello django! whts up")
